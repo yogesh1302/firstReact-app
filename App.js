32ad8025b10029f5c829af670487fc232a4bef3d
YogeshPawar <yogeshpawar@crescentdigital.io>
2026-01-07 12:03:30 +1100

    modified package json

--- App.js
+++ App.js
@@ -10,10 +10,11 @@
 </div>
 
 */
-const parent = React.createElement("div",{id : "parent"},
-     React.createElement("div",{id : "child"},React.createElement("h1",{},"I am h1 tag")));
-     console.log(parent);
-//const heading = React.createElement("h1",{ id :"heading", xyz : "abc"},"Hello World from Reactr");
-    const root= ReactDOM.createRoot(document.getElementById("root"));
+// ...existing code...
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-root.render(parent);
+const heading = React.createElement("h1",{ id :"heading", xyz : "abc"},"Hello World from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+// ...existing code...
